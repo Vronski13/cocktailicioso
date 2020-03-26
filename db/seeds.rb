316e06fefdfb103c2041a5e39b7f81354b303a30
Verena Mueller <verena.mller@gmail.com>
2020-03-26 15:48:06 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktail_serialized = open(url).read
+cocktail = JSON.parse(cocktail_serialized)
+
+puts "Creating Ingredients"
+cocktail['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+puts "Creating Long Island Ice Tea"
+file = URI.open('https://images.unsplash.com/photo-1510626176961-4b57d4fbad03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80')
+ice = Cocktail.new(name: "Long Island Iced Tea")
+ice.photo.attach(io: file, filename: '2.png', content_type: 'image/png')
+ice.save
+
+puts "Creating Manhattan"
+file = URI.open('https://images.unsplash.com/photo-1576874240748-795e3bfa4c5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+manhattan = Cocktail.new(name: "Manhattan")
+manhattan.photo.attach(io: file, filename: '3.png', content_type: 'image/png')
+manhattan.save
+
+puts "Creating daiquiri"
+file = URI.open('https://images.unsplash.com/photo-1566733971017-f6a46e832e95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+daiquiri = Cocktail.new(name: "Daiquiri")
+daiquiri.photo.attach(io: file, filename: '4.png', content_type: 'image/png')
+daiquiri.save
+
+puts "Creating margarita"
+file = URI.open('https://images.unsplash.com/photo-1500420986994-e4ad8f630fee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+margarita = Cocktail.new(name: "Margarita")
+margarita.photo.attach(io: file, filename: '5.png', content_type: 'image/png')
+margarita.save
+
+puts "bloody mary"
+file = URI.open('https://images.unsplash.com/photo-1571045741139-afb07c44b2c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+bloody = Cocktail.new(name: "Bloody Mary")
+bloody.photo.attach(io: file, filename: '6.png', content_type: 'image/png')
+bloody.save
+
+puts "Finished"
