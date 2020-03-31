4fd466d451074d1c0729fea06ef03893a44f056e
Verena Mueller <verena.mller@gmail.com>
2020-03-31 18:31:49 +0200

    solved validation conflicts in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :cocktails
   has_many :reviews
-  validates :username, presence: true, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
