4210119fc2f76f60a4e9fa0929207be26e754acb
Verena Mueller <verena.mller@gmail.com>
2020-03-31 18:15:28 +0200

    generated review model and dependencies between user, cocktails and reviews

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,8 @@ class Cocktail < ApplicationRecord
   has_one_attached :photo
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_many :reviews
+  belongs_to :user
   validates :name, presence: true, uniqueness: true
   include PgSearch::Model
   multisearchable against: :name
