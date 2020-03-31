971b1f87e3501cd6eb741c3be6d68f88a1e09c36
Verena Mueller <verena.mller@gmail.com>
2020-03-31 18:22:35 +0200

    added username to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :cocktails
   has_many :reviews
+  validates :username, presence: true, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
